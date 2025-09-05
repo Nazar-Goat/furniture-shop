1fea2e1991ea927c2e67a4bf90e111b9d5336af0
Nazar Kozlovsky <zatsepylonazar@gmail.com>
2025-09-05 16:49:19 +0300

    Added static files and work on frontend parts of application

--- main/views.py
+++ main/views.py
@@ -4,13 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'title': 'Main page',
-        'content': 'Main page of the internet shop - HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'bool': False
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
+        
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Main - о нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том почему этот товар такой классный и как он всем нравится'
+    
+    }
+    return render(request, 'main/about.html', context)
