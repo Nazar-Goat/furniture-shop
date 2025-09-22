40b6703f64135f2511d6f654426be7cce9f84e9d
Nazar Kozlovsky <zatsepylonazar@gmail.com>
2025-09-22 12:49:25 +0300

    Cart app . All cart templates are included

---
+++ carts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def cart_add(request, product_id):
+    None
+
+def cart_change(request, product_id):
+    None
+
+def cart_remove(request, product_id):
+    None
