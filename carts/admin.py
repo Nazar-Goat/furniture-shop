c34e3da7edbbc51d23c1049df3e17e6e766253f2
Nazar Kozlovsky <zatsepylonazar@gmail.com>
2025-09-29 17:12:47 +0300

    Admin panel setup

--- carts/admin.py
+++ carts/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from carts.models import Cart
 
-# Register your models here.
-admin.site.register(Cart)
+class CartTabAdmin(admin.TabularInline):
+    model= Cart
+    fields= 'product', 'quantity', 'created_timestamp'
+    search_fields= 'product', 'quantity', 'created_timestamp'
+    readonly_fields= ('created_timestamp', )
+    extra= 1
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display= ['display_user', 'display_product', 'quantity', 'created_timestamp']
+    list_filter= ['created_timestamp', 'user', 'product__name']
+
+    def display_user(self, obj):
+        if obj.user:
+            return str(obj.user)
+        return "Аннонимный пользователь"
+    
+    def display_product(self, obj):
+        return str(obj.product.name)
